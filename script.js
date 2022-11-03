09e3e70e8ff36460694b83857b8c26261ad33d2e
femas1 <federico.massara2@gmail.com>
2022-11-03 18:45:51 +0100

    getting bars in js

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ fetch('./data.json')
         console.log(data[0])
     })
 
-
-
 // GETTING THE BARS
 
 const monBar = document.querySelector('.container__main__heading__days__mon__bar');
+const tueBar = document.querySelector('.container__main__heading__days__tue__bar');
+const wedBar = document.querySelector('.container__main__heading__days__wed__bar');
+const thuBar = document.querySelector('.container__main__heading__days__thu__bar');
+const friBar = document.querySelector('.container__main__heading__days__fri__bar');
+const satBar = document.querySelector('.container__main__heading__days__sat__bar');
+const sunBar = document.querySelector('.container__main__heading__days__sun__bar');
 
