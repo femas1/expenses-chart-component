7744e6c75591d75978c8b1ae4754081c0402bb7e
femas1 <federico.massara2@gmail.com>
2022-11-03 19:04:58 +0100

    getting day values from json and setting highest value for css height

--- script.js
+++ script.js
@@ -3,7 +3,18 @@
 fetch('./data.json')
     .then((res) => res.json())
     .then((data) => {
-        console.log(data[0])
+        let highestValue = 150;
+
+        let monValue = data[0].amount;
+        let tueValue = data[1].amount;
+        let wedValue = data[2].amount;
+        let thuValue = data[3].amount;
+        let friValue = data[4].amount;
+        let satValue = data[5].amount;
+        let sunValue = data[6].amount;
+        
+        
+        
     })
 
 // GETTING THE BARS
@@ -16,4 +27,6 @@ const friBar = document.querySelector('.container__main__heading__days__fri__bar
 const satBar = document.querySelector('.container__main__heading__days__sat__bar');
 const sunBar = document.querySelector('.container__main__heading__days__sun__bar');
 
+
 monBar.style.height = "100px"
+
